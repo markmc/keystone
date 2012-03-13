7795d9e17f546fde46c9382718734042603092f8
Mark McLoughlin <markmc@redhat.com>
2012-03-13 14:27:18 +0000

    Remove cfg wrappers from keystone.config
    The idea with the cfg module is that we can have all projects using not
    only the same code for config, but using similar idioms that will be
    familiar to anyone working on OpenStack.
    
    The register_*() wrappers don't add much in the way of convenience, but
    if we really want them, we can add them to cfg itself.
    
    Subsequent commits in this series remove the global CONF variable and
    move option definitions closer to where the option is used. The end
    result is worth considering as well as this initial baby step.
    
    Change-Id: Ia8e11592417fa67cf3682e494653b8e6ba166244

--- keystone/config.py
+++ keystone/config.py
@@ -92,92 +92,55 @@ def setup_logging(conf):
     root_logger.addHandler(handler)
 
 
-def register_str(*args, **kw):
-    conf = kw.pop('conf', CONF)
-    group = _ensure_group(kw, conf)
-    return conf.register_opt(cfg.StrOpt(*args, **kw), group=group)
-
-
-def register_cli_str(*args, **kw):
-    conf = kw.pop('conf', CONF)
-    group = _ensure_group(kw, conf)
-    return conf.register_cli_opt(cfg.StrOpt(*args, **kw), group=group)
-
-
-def register_bool(*args, **kw):
-    conf = kw.pop('conf', CONF)
-    group = _ensure_group(kw, conf)
-    return conf.register_opt(cfg.BoolOpt(*args, **kw), group=group)
-
-
-def register_cli_bool(*args, **kw):
-    conf = kw.pop('conf', CONF)
-    group = _ensure_group(kw, conf)
-    return conf.register_cli_opt(cfg.BoolOpt(*args, **kw), group=group)
-
-
-def register_int(*args, **kw):
-    conf = kw.pop('conf', CONF)
-    group = _ensure_group(kw, conf)
-    return conf.register_opt(cfg.IntOpt(*args, **kw), group=group)
-
-
-def register_cli_int(*args, **kw):
-    conf = kw.pop('conf', CONF)
-    group = _ensure_group(kw, conf)
-    return conf.register_cli_opt(cfg.IntOpt(*args, **kw), group=group)
-
-
-def _ensure_group(kw, conf):
-    group = kw.pop('group', None)
-    if group:
-        conf.register_group(cfg.OptGroup(name=group))
-    return group
-
-
 CONF = CommonConfig(project='keystone')
 
+global_opts = [
+    cfg.StrOpt('admin_token', default='ADMIN'),
+    cfg.StrOpt('bind_host', default='0.0.0.0'),
+    cfg.StrOpt('compute_port'),
+    cfg.StrOpt('admin_port'),
+    cfg.StrOpt('public_port'),
+    ]
 
-register_str('admin_token', default='ADMIN')
-register_str('bind_host', default='0.0.0.0')
-register_str('compute_port')
-register_str('admin_port')
-register_str('public_port')
+CONF.register_opts(global_opts)
 
+sql_opts = [
+    cfg.StrOpt('connection'),
+    cfg.StrOpt('idle_timeout'),
+    cfg.StrOpt('min_pool_size'),
+    cfg.StrOpt('max_pool_size'),
+    cfg.StrOpt('pool_timeout'),
+    ]
 
-# sql options
-register_str('connection', group='sql')
-register_str('idle_timeout', group='sql')
-register_str('min_pool_size', group='sql')
-register_str('max_pool_size', group='sql')
-register_str('pool_timeout', group='sql')
+CONF.register_group(cfg.OptGroup('sql'))
+CONF.register_opts(sql_opts, group='sql')
 
+ldap_opts = [
+    cfg.StrOpt('url'),
+    cfg.StrOpt('user'),
+    cfg.StrOpt('password'),
+    cfg.StrOpt('suffix'),
+    cfg.BoolOpt('use_dumb_member'),
 
-register_str('driver', group='catalog')
-register_str('driver', group='identity')
-register_str('driver', group='policy')
-register_str('driver', group='token')
-register_str('driver', group='ec2')
+    cfg.StrOpt('user_tree_dn'),
+    cfg.StrOpt('user_objectclass'),
+    cfg.StrOpt('user_id_attribute'),
 
-
-#ldap
-register_str('url', group='ldap')
-register_str('user', group='ldap')
-register_str('password', group='ldap')
-register_str('suffix', group='ldap')
-register_bool('use_dumb_member', group='ldap')
-
-register_str('user_tree_dn', group='ldap')
-register_str('user_objectclass', group='ldap')
-register_str('user_id_attribute', group='ldap')
-
-register_str('tenant_tree_dn', group='ldap')
-register_str('tenant_objectclass', group='ldap')
-register_str('tenant_id_attribute', group='ldap')
-register_str('tenant_member_attribute', group='ldap')
+    cfg.StrOpt('tenant_tree_dn'),
+    cfg.StrOpt('tenant_objectclass'),
+    cfg.StrOpt('tenant_id_attribute'),
+    cfg.StrOpt('tenant_member_attribute'),
 
 
-register_str('role_tree_dn', group='ldap')
-register_str('role_objectclass', group='ldap')
-register_str('role_id_attribute', group='ldap')
-register_str('role_member_attribute', group='ldap')
+    cfg.StrOpt('role_tree_dn'),
+    cfg.StrOpt('role_objectclass'),
+    cfg.StrOpt('role_id_attribute'),
+    cfg.StrOpt('role_member_attribute'),
+    ]
+
+CONF.register_group(cfg.OptGroup('ldap'))
+CONF.register_opts(ldap_opts, group='ldap')
+
+for backend in ['catalog', 'identity', 'policy', 'token', 'ec2']:
+    CONF.register_group(cfg.OptGroup(backend))
+    CONF.register_opt(cfg.StrOpt('driver'), group=backend)
